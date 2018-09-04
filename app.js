cd7a7c50eb70415f439c0b97c6db2fa861e09bb4
Kimberly <leubnerk@otc.edu>
2018-09-04 11:53:43 -0500

    Created app.js
    Code is functional!

---
+++ app.js
@@ -0,0 +1,25 @@
+/* 
+    Kimberly Leubner
+    CIS 131
+    Javascript Practice 
+*/
+
+// My name and age
+console.log("My name is Kimberly Leubner!");
+console.log("I am 28 years old!");
+
+// Cacluating 6 and 3
+console.log("\n6 + 3 = ", 6 + 3);
+console.log("6 - 3 = ", 6 - 3);
+console.log("6 x 3 = ", 6 * 3);
+console.log("6 / 3 = ", 6/3);
+
+// Predictions
+console.log("\n",4 + 5);     // I predict this will display 9.
+console.log("4 + 5");   // I predict this will display 4 + 5.
+console.log("4" + "5"); // I predict this will display 45.
+
+// Temperature Conversion
+const c = 32;
+const convert = c * 9/5 + 32;
+console.log("\n32 degrees Celsius in Fahrenheit is " + convert + " degrees.");
